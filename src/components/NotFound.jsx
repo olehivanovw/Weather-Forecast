61ecfaa4fb02257b8a88d4a4dd200602982748d8
olehivanovw <olehivanovw@gmail.com>
2023-12-29 20:29:39 +0200

    Added first style for '404' pages use "Material UI".

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,13 +1,14 @@
 import {Link} from "react-router-dom";
+import {Typography, Box, Button} from "@mui/material";
 
 export default function NotFound() {
   return (
-    <div>
-      <h1>Page Not Found</h1>
-      <h1>404</h1>
+    <Box sx={{textAlign: 'center'}}>
+      <Typography variant="h3" sx={{mb: 2}}>404 - Page not found</Typography>
+      <Typography variant="body1" sx={{mb: 2}}>We're sorry, but the page you requested cannot be found.</Typography>
       <Link to='/'>
-        <button>BACK TO SEARCH</button>
+        <Button variant="outlined">Back to search</Button>
       </Link>
-    </div>
+    </Box>
   )
 }
