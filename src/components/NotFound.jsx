5548a3a376b19740685f26a3a645f30cb056a91b
olehivanovw <olehivanovw@gmail.com>
2024-01-02 18:50:34 +0200

    Added image for "404" page.

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,11 +1,13 @@
 import {Link} from "react-router-dom";
 import {Typography, Box, Button} from "@mui/material";
+import errorImg from "../assets/404-error.png";
 
 export default function NotFound() {
   return (
     <Box sx={{textAlign: 'center'}}>
-      <Typography variant="h3" sx={{mb: 2}}>404 - Page not found</Typography>
-      <Typography variant="body1" sx={{mb: 2}}>We're sorry, but the page you requested cannot be found.</Typography>
+      <Box component='img' src={errorImg} alt="error-404" sx={{width: '60%' }}></Box>
+      <Typography variant="h4" sx={{mb: 2}}>Page not found</Typography>
+      <Typography variant="body2" sx={{mb: 2}}>We're sorry, but the page you requested cannot be found.</Typography>
       <Link to='/'>
         <Button variant="outlined">Back to search</Button>
       </Link>
