861e031d9a97a13762db691af919a356a5956dea
olehivanovw <olehivanovw@gmail.com>
2024-01-11 23:29:39 +0200

    Added new background image on main page. I continue experiments with material design.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,23 +2,13 @@ import {Routes, Route} from "react-router-dom";
 import Form from "./Form.jsx";
 import Weather from "./Weather";
 import NotFound from "./NotFound.jsx";
-import {Container} from "@mui/material";
 
 export default function App() {
   return (
-      <Container maxWidth="md" sx={
-        {
-          minHeight: '100vh',
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }
-      }>
-        <Routes>
-          <Route path='/' element={<Form />}></Route>
-          <Route path='/weather/:city' element={<Weather />}></Route>
-          <Route path='/*' element={<NotFound />}></Route>
-        </Routes>
-      </Container>
+    <Routes>
+      <Route path='/' element={<Form />}></Route>
+      <Route path='/weather/:city' element={<Weather />}></Route>
+      <Route path='/*' element={<NotFound />}></Route>
+    </Routes>
   )
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,45 @@
 import {useNavigate} from "react-router-dom";
-import {Typography, Button, Box, TextField, Paper} from "@mui/material";
+import {Typography, Button, Box, TextField, Paper, Container, styled} from "@mui/material";
+import backImgMain from "../assets/backImgMain.jpg"
+
+const styles = {
+  container: {
+    minHeight: '100vh',
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundImage: `url(${backImgMain})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'top',
+  },
+  paperBox: {
+    textAlign: 'center',
+    paddingTop: 6,
+    paddingLeft: 8,
+    paddingRight: 8,
+    paddingBottom: 6,
+    borderRadius: 2,
+    bgcolor: 'rgba(232,232,232,0.4)'
+  },
+  typography: {
+    mb: 4,
+    fontWeight: 'bold',
+    fontSize: '5rem',
+  },
+  formBox: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 2
+  },
+  textField: {
+    mr: 2,
+    maxWidth: '25ch',
+  },
+  button: {
+    fontSize: '1rem'
+  }
+};
 
 export default function Form() {
   const navigate = useNavigate()
@@ -13,25 +53,14 @@ export default function Form() {
   }
 
   return (
-    <Box component={Paper} elevation={3} sx={
-      {
-        textAlign: 'center',
-        padding: 4,
-        borderRadius: 4,
-      }
-    }>
-      <Typography variant="h1" sx={{mb: 4, fontWeight: 'medium'}}>Weather Forecast</Typography>
-      <Box component="form" onSubmit={handleSearch} sx={
-        {
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          gap: 2
-        }
-      }>
-        <TextField id="outlined-basic" label="Enter city" variant="outlined" name='inpCity' size="small" sx={{ mr: 2, width: '25ch'}}/>
-        <Button variant="contained" type='submit'>Search</Button>
+    <Container maxWidth="xl" sx={styles.container}>
+      <Box component={Paper} elevation={3} sx={styles.paperBox}>
+        <Typography variant="h1" sx={styles.typography}>Weather Forecast</Typography>
+        <Box component="form" onSubmit={handleSearch} sx={styles.formBox}>
+          <TextField label="Enter city" variant="outlined" name='inpCity' size="small" autoComplete="off" sx={styles.textField}/>
+          <Button variant="contained" type='submit' sx={styles.button}>Search</Button>
+        </Box>
       </Box>
-    </Box>
+    </Container>
   )
 }
