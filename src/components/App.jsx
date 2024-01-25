bcc1c94943beb2d9dabfe1bc563b372baff50ff6
olehivanovw <olehivanovw@gmail.com>
2024-01-25 21:03:30 +0200

    Added new code for optimization pages. This is 'Suspense' and 'lazy'.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,25 @@
 import {Routes, Route} from "react-router-dom";
-import Form from "./Form.jsx";
-import Weather from "./Weather";
-import NotFound from "./NotFound.jsx";
+// стандартний спосіб використання та підгруження компонентів
+// import Form from "./Form.jsx";
+// import Weather from "./Weather";
+// import NotFound from "./NotFound.jsx";
+import {lazy, Suspense} from 'react'
+import Load from './Load.jsx'
+
+// Використання lazy для підгрузки коли цей компонент буде рендеритися вперше.
+const FormAsync = lazy(() => import('./Form.jsx'))
+const WeatherAsync = lazy(() => import('./Weather.jsx'))
+const NotFoundAsync = lazy(() => import('./NotFound.jsx'))
 
 export default function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Form />}></Route>
-      <Route path='/weather/:city' element={<Weather />}></Route>
-      <Route path='/*' element={<NotFound />}></Route>
-    </Routes>
+    // Використання Suspense для відображання резервного варіанту, доки його дочірні елементи не завершать завантаження.
+    <Suspense fallback={<Load />}>
+      <Routes>
+        <Route path='/' element={<FormAsync />}></Route>
+        <Route path='/weather/:city' element={<WeatherAsync />}></Route>
+        <Route path='/*' element={<NotFoundAsync />}></Route>
+      </Routes>
+    </Suspense>
   )
 }
