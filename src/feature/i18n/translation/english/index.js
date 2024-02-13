7324168e794291a77917e45905e6881664f67fda
olehivanovw <olehivanovw@gmail.com>
2024-02-13 22:09:20 +0200

    Added setup internationalization i18n with languages.

--- src/feature/i18n/translation/english/index.js
+++ src/feature/i18n/translation/english/index.js
@@ -1,5 +1,23 @@
 const en = {
-  "Welcome to React": "Welcome to React"
+  logo: 'Weather',
+  headingMain: 'Weather Forecast',
+  inputPlaceholder: 'Enter city',
+  button: {
+    search: 'SEARCH',
+    back: 'BACK TO SEARCH',
+    lang: {
+      en: 'EN',
+      uk: 'UK',
+    }
+  },
+  load: 'Please Wait...',
+  notFound: {
+    ops: 'OOOps!',
+    page: 'Page not found!',
+    sorry: 'We\'re sorry, but the page you requested cannot be found!',
+  },
+  speed: '{{speed}} m/s',
+  rulesSearch: 'Important: The search request can only be made in English!',
 }
 
 export default en
