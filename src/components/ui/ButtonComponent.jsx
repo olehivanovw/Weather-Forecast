cd7204fc0073ab6a961bfe8eb10ab258bd069ef6
olehivanovw <olehivanovw@gmail.com>
2024-02-13 21:06:51 +0200

    Added new attributes in props.

--- src/components/ui/ButtonComponent.jsx
+++ src/components/ui/ButtonComponent.jsx
@@ -3,9 +3,10 @@ import { memo } from 'react'
 
 // Використання незалежного компонента для його перевикористанні в проекті та більшої гнучності додатку.
 // 'memo' дозволяє пропустити повторне рендеринг компонента, якщо його атрибути не змінені.
-export const ButtonComponent = memo(({children, variant, type, sx, disabled, href}) => {
+export const ButtonComponent = memo(({children, variant, type, sx, disabled, href, onClick}) => {
   return (
     <Button
+      onClick={onClick}
       variant={variant}
       type={type}
       sx={sx}
